ff97008cda7735435c41dd7e0130755b019bd503
tanvir35web <islammdtanvirul@gmail.com>
2024-01-01 01:00:34 +0600

    stop action add conditionally

--- src/components/TimerChallenges.jsx
+++ src/components/TimerChallenges.jsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 
-export default function TimerChallenges( {title, targetTime } ) {
+export default function TimerChallenges({ title, targetTime }) {
+  const timer = useRef();
 
   const [timeStarted, setTimeStarted] = useState(false);
   const [timeExpired, SetTimeExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       SetTimeExpired(true);
     }, targetTime * 1000);
 
     setTimeStarted(true);
   }
 
+  function handleStop() {
+    clearTimeout(timer.current)
+  }
+
   return (
-    <section className='challenge'>
-        <h2>{title}</h2>
-        {timeExpired && <p>You lost!</p>}
-        <p className='challenges-time'>
-            {targetTime} second{targetTime > 1 ? "s" : ""}
-        </p>
-        <p>
-          <button onClick={handleStart}>
-            {timeStarted ? "Start" : undefined} Challenge
-          </button>
-        </p>
-        <p className=''>Time is Running... / Timer Inactive</p>
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timeExpired && <p>You lost!</p>}
+      <p className="challenges-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timeStarted ? handleStop : handleStart}>
+          {timeStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timeStarted ? "active" : undefined}>
+        {timeStarted ? "Time is Running..." : "Timer Inactive"};
+      </p>
     </section>
-  )
+  );
 }
